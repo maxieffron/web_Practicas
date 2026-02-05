0cb1bffbb42ef41f5c959aecd8a625a56d79a8f1
maxieffron <mleffron86@gmail.com>
2026-02-05 01:32:15 -0300

    Se actualiza el CRUD de la clase 7

--- JAVASCRIPT/EJERCICIOS/CLASE 7 - FUNCIONES DE ORDEN SUPERIOR/assets/js/ejercicio1.js
+++ JAVASCRIPT/EJERCICIOS/CLASE 7 - FUNCIONES DE ORDEN SUPERIOR/assets/js/ejercicio1.js
@@ -21,7 +21,6 @@ const usuarios = [usuario1,usuario2,usuario3,usuario4,usuario5,usuario6]
 console.log("INICIAL",usuarios)
 
 let option = 0
-let id = 6
 let UsuarioEncontrado
 let userSerach 
 
@@ -37,7 +36,7 @@ function mostrarMenu()
                                 3. Modificar Usuario
                                 4. Mostrar Usuarios
                                 5. Buscar Usuario
-                                6. Mostrar NOMBRE + APELLIDO
+                                6. Filtrar por NOMBRE y/o APELLIDO
                                 10. Salir         
                               `))
 
@@ -48,8 +47,7 @@ function mostrarMenu()
 
                 const nombre = prompt("Ingrese un Nombre")
                 const apellido = prompt("Ingrese un Apellido")
-                id++
-                agregarUsuario(id,nombre,apellido)
+                agregarUsuario(nombre,apellido)
 
                 break; 
 
@@ -69,15 +67,15 @@ function mostrarMenu()
             break;
 
             case 3: //Modificar Usuario
+
+                modificarUsuario
             
             break;
 
             case 4: //Mostrar Usuarios
-                for (let usu of usuarios)
-                {
-                    console.log(`Usuario con: id: ${usu.id}, Nombre: ${usu.nombre}, Apellido: ${usu.apellido}`)
-                }
-            
+                
+                mostrarUsuarios()
+                
             break;
 
             case 5: //Buscar Usuario
@@ -88,11 +86,26 @@ function mostrarMenu()
                 
             break;
 
-            case 6: //Mostrar NOMBRE + APELLIDO
+            case 6: //Filtrar por NOMBRE y APELLIDO
                 
-            for (let usu of usuarios)
+                const usuariosFiltrados = filtrarUsuarios()
+
+                if (usuariosFiltrados.length > 0)
                 {
-                    console.log(`Usuario con Nombre: ${usu.nombre} y Apellido: ${usu.apellido}`)
+                    usuariosFiltrados.forEach((usu) =>{
+
+                        console.table(`Usuario encontrado:
+                        Id: ${usu.id}
+                        Nombre: ${usu.nombre}
+                        Apellido: ${usu.apellido}
+                        `)
+
+                })
+                    
+                }
+                else
+                {
+                    alert(`No hay usuarios que cumplan con el filtro ingresado`)    
                 }
             
             break;
@@ -113,14 +126,34 @@ function mostrarMenu()
 }
 
 //function agregarUsuario(usuarios.push(new Usuario(id+1,nombre,apellido)))
-function agregarUsuario(id,nombre,apellido)
+function agregarUsuario(nombre,apellido)
 {
-    usuarios.push(new Usuario(id,nombre,apellido))
+    usuarios.push(new Usuario(getId(),nombre,apellido))
+    /*
     return alert(`Se ha agregado el Usuario con:
     id: ${id}
     Nombre: ${nombre}
     Apellido: ${apellido}`)
+    */
+
+   console.table(`Nuevo Usuario --> Id: ${usuarios[usuarios.length -1].id} , Nombre: ${usuarios[usuarios.length -1].nombre}, Apellido: ${usuarios[usuarios.length -1].apellido}`)
 }
+
+function getId()
+{
+    if (usuarios.length ===0)
+    {
+        //Si el array está vacío, devuelvo un 1
+        return 1
+    }
+    else
+    {
+        //Calculo el último id y le sumo 1
+        const ultimoId = usuarios[usuarios.length -1]
+        return ultimoId.id + 1
+    }
+}
+
 
 function borrarUsuario(indiceUser)
 {
@@ -130,15 +163,39 @@ function borrarUsuario(indiceUser)
     }
     else
     {
-        alert(`Se ha eliminado el Usuario ${usuarios[indiceUser].nombre}`)
+        //alert(`Se ha eliminado el Usuario ${usuarios[indiceUser].nombre}`)
+
+        console.table(`Usuario Eliminado --> Id: ${usuarios[indiceUser].id} , Usuario: ${usuarios[indiceUser].nombre}, Nombre: ${usuarios[indiceUser].apellido}`)
+
         usuarios.splice(indiceUser,1)    
     }
     
 }
 
+
+function mostrarUsuarios()
+{
+    
+    //FORMA Nº1 DE RECORRER EL ARRAY
+    /*
+    for (let usu of usuarios)
+    {
+        console.log(`Usuario con: id: ${usu.id}, Nombre: ${usu.nombre}, Apellido: ${usu.apellido}`)
+    }
+    */
+
+    //FORMA Nº2 DE RECORRER EL ARRAY
+    usuarios.forEach((usu) =>{
+        console.log(`Usuario con --> id: ${usu.id}, Nombre: ${usu.nombre}, Apellido: ${usu.apellido}`)
+    })
+
+
+
+}
+
 function BuscarUsuario(Usu)
 {
-    UsuarioEncontrado = usuarios.find((elem) => elem.nombre === userSerach)
+    UsuarioEncontrado = usuarios.find((elem) => elem.nombre === Usu)
     if ( UsuarioEncontrado === undefined)
     {
         alert(`El Usuario ${userSerach} NO EXISTE`)
@@ -151,4 +208,23 @@ function BuscarUsuario(Usu)
         Nombre: ${UsuarioEncontrado.nombre}
         Apellido: ${UsuarioEncontrado.apellido}`)
     }
+}
+
+function filtrarUsuarios()
+{
+    let textSearch = prompt("Ingrese un texto a buscar")
+
+    //Esto retorna un array con todo lo que cumple con el filtro
+    return usuarios.filter((usuario) => 
+
+        //Buscamos si la cadena ingresada forma parte de algún nombre en el arrary
+        //Si no existe, devuelve -1
+        usuario.nombre.indexOf(textSearch) !== -1 ||
+        
+        //Buscamos si la cadena ingresada forma parte de algún apellido en el arrary
+        //Si no existe, devuelve -1
+        usuario.apellido.indexOf(textSearch) !== -1
+
+    )
+
 }
